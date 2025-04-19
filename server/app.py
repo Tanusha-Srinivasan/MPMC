645cfbac10f8cdd25829dbf4c3dfb0447eb11975
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-04-19 19:26:51 +0530

    Flask app basic

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from app import create_app
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+
+app = create_app()
+CORS(app)
+
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0' ,port = 8000)
